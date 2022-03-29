c8d509730acbe056aa1e82ebece6292db23c03b6
Papoel <pascal.briffard@gmail.com>
2022-03-29 10:42:05 +0200

    Add User lambda

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -15,20 +15,36 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        $userAdmin = new User();
+
+        $userAdmin->setFirstname('Pascal');
+        $userAdmin->setLastname('Briffard');
+        $userAdmin->setAddress('12 rue des indécis');
+        $userAdmin->setZip('59000');
+        $userAdmin->setTown('Lille');
+        $userAdmin-> setEmail("admin@email.fr");
+        $userAdmin-> setRoles(["ROLE_ADMIN"]);
+        $hash = $this->passwordHasher->hashPassword($userAdmin, ('password123'));
+        $userAdmin->setPassword($hash);
+        $userAdmin->setIsAdmin(true);
+
+        $manager->persist($userAdmin);
+
         $user = new User();
 
-        $user->setFirstname('Pascal');
-        $user->setLastname('Briffard');
-        $user->setAddress('12 rue des indécis');
-        $user->setZip('59000');
-        $user->setTown('Lille');
-        $user-> setEmail("admin@admin.fr");
-        $user-> setRoles(["ROLE_ADMIN"]);
-        $hash = $this->passwordHasher->hashPassword($user, ('password'));
+        $user->setFirstname('John');
+        $user->setLastname('Doe');
+        $user->setAddress('126 rue de Victor Hugo');
+        $user->setZip('07000');
+        $user->setTown('Cruas');
+        $user-> setEmail("user@email.fr");
+        $user-> setRoles(["ROLE_USER"]);
+        $hash = $this->passwordHasher->hashPassword($user, ('password456'));
         $user->setPassword($hash);
         $user->setIsAdmin(true);
 
         $manager->persist($user);
+
         $manager->flush();
     }
 }
