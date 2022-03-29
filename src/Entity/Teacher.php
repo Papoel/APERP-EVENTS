44c165204770cb3eeb857566b0d89ec25dae966c
Papoel <pascal.briffard@gmail.com>
2022-03-29 11:26:06 +0200

    Add Entity Teacher

---
+++ src/Entity/Teacher.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeacherRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeacherRepository::class)]
+class Teacher
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    private $firstname;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $lastname;
+
+    #[ORM\ManyToMany(targetEntity: Classroom::class, inversedBy: 'teachers')]
+    private $classroom;
+
+    public function __construct()
+    {
+        $this->classroom = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Classroom>
+     */
+    public function getClassroom(): Collection
+    {
+        return $this->classroom;
+    }
+
+    public function addClassroom(Classroom $classroom): self
+    {
+        if (!$this->classroom->contains($classroom)) {
+            $this->classroom[] = $classroom;
+        }
+
+        return $this;
+    }
+
+    public function removeClassroom(Classroom $classroom): self
+    {
+        $this->classroom->removeElement($classroom);
+
+        return $this;
+    }
+}
