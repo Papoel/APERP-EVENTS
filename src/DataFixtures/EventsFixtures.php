93861f1019cec6a6e97fa1d1d4dc728da1b68877
Papoel <pascal.briffard@gmail.com>
2022-03-30 22:19:51 +0200

    Add Entity Events and her DataFixtures (without relation)

---
+++ src/DataFixtures/EventsFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Events;
+use Faker;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class EventsFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 20; $i++) {
+
+            $event = new Events();
+
+            $event->setName($faker->sentence(5));
+            $event->setLocation($faker->secondaryAddress());
+            $event->setPrice($faker->randomFloat(0, 49.50));
+            $event->setCapacity($faker->numberBetween(10, 150));
+            $event->setDescription($faker->realTextBetween($minNbChars = 80, $maxNbChars = 500, $indexSize = 2));
+            $event->setImage($faker->imageUrl(640, 480, 'animals', true));
+
+            $manager->persist($event);
+
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Events.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventsRepository::class)]
+class Events
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $location;
+
+    #[ORM\Column(type: 'float', nullable: true)]
+    private $price;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $capacity;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(?int $capacity): self
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
